ac3d8b42e509020ebc5b6842fb194b390e64c006
dcinick78 <dcinick78@gmail.com>
2019-07-08 22:43:23 +0200

    update2

--- src/EditBox.js
+++ src/EditBox.js
@@ -3,30 +3,24 @@ class TextBox extends React.Component{
     constructor(props){
         super(props)
         this.state={
-            boxTitle:'Box',
-            editState:true,
+            boxTitle:this.props.boxId+' Box',
+            editState:this.props.editableX,
             postBody:'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architectodsasd'
         }
     }
     static getDerivedStateFromProps(props,state){    }
-    componentDidMount(){}
-    componentDidUpdate(){
-
-    }
-    componentWillUnmount(){}
-    editBox = () => {
-        this.setState(prevState=>({editState:!prevState.editState}));
-        console.log(this.state.editState)
-    }
     updateTextBody = (e) => {
         this.setState({postBody: e.target.value});
     }
+    editBoxBox = () => {
+        this.setState(prevState=>({editState:!prevState.editState}));
+      }
     render(){
         return (
-            <div className="PostCard__id">
+            <div className="PostCard__id" >
                 <h1>{this.state.boxTitle}</h1>
                 {this.state.editState ? <textarea cols="10" onChange={(e)=>this.updateTextBody(e)} className="PostCard_area__id">{this.state.postBody}</textarea> : <p>{this.state.postBody}</p>}                
-                <button className="PostCard_edit__id" onClick={()=>this.editBox()}>Edit</button>
+                <button className="PostCard_edit__id" onClick={()=>this.editBoxBox()}>Edit</button>
             </div>
         )
     }
