bcbca112bb3793d9c50e53154989287ee0f5b1f6
dcinick78 <dcinick78@gmail.com>
2019-07-09 22:38:55 +0200

    done

--- src/App.js
+++ src/App.js
@@ -17,12 +17,12 @@ class App extends React.Component {
       }
   }
   editBox = () => {
-    this.setState(prevState=>({editState:!prevState.editState}));
+    this.setState({editState:!this.state.editState});
   }
   render() {
     return (
-      <div className="App">
-      <EditBox boxId={this.state.data[0].name} editableX={this.state.editState} editBox={this.editBox}/>
+      <div className={"App"}>
+      <EditBox boxId={this.state.data[0].name} editableX={this.state.editState} editBox={this.editBox} />
       <EditBox boxId={this.state.data[1].name} editableX={this.state.editState} editBox={this.editBox} />
       <EditBox boxId={this.state.data[2].name} editableX={this.state.editState} editBox={this.editBox} />
       <EditBox boxId={this.state.data[3].name} editableX={this.state.editState} editBox={this.editBox} />
--- src/EditBox.js
+++ src/EditBox.js
@@ -4,25 +4,30 @@ class TextBox extends React.Component{
         super(props)
         this.state={
             boxTitle:this.props.boxId+' Box',
-            editState:this.props.editableX,
             postBody:'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architectodsasd'
         }
     }
-    static getDerivedStateFromProps(props,state){    }
     updateTextBody = (e) => {
         this.setState({postBody: e.target.value});
     }
-    editBoxBox = () => {
-        this.setState(prevState=>({editState:!prevState.editState}));
-      }
     render(){
-        return (
-            <div className="PostCard__id" >
+        if (this.props.editableX) {
+            return(
+                <div className="PostCard__id" >
                 <h1>{this.state.boxTitle}</h1>
-                {this.state.editState ? <textarea cols="10" onChange={(e)=>this.updateTextBody(e)} className="PostCard_area__id">{this.state.postBody}</textarea> : <p>{this.state.postBody}</p>}                
-                <button className="PostCard_edit__id" onClick={()=>this.editBoxBox()}>Edit</button>
-            </div>
-        )
+                <textarea cols="10" onChange={(e)=>this.updateTextBody(e)} className="PostCard_area__id" value={this.state.postBody}></textarea>
+                <button className="PostCard_edit__id" onClick={()=>this.props.editBox()}>Edit</button>
+                </div>
+            )
+        } else {
+            return(
+                <div className="PostCard__id" >
+                <h1>{this.state.boxTitle}</h1>
+                <p>{this.state.postBody}</p>
+                <button className="PostCard_edit__id" onClick={()=>this.props.editBox()}>Edit</button>
+                </div>
+            )
+        }
     }
 }
 export default TextBox;
